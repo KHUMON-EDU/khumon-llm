3bab5c00967bbe3c5a54706d094a45f8e5c7c0cc
FacerAin <ywsong.dev@kakao.com>
2023-10-19 10:17:49 +0900

    [#6] feat: add live mode to test api

--- app/api/api_v1/endpoints/generation.py
+++ app/api/api_v1/endpoints/generation.py
@@ -3,32 +3,41 @@ from typing import Any
 from fastapi import APIRouter, UploadFile
 
 from app.generation import Generator
-from app.schemas.generation import Generation, ReuqestText
+from app.schemas.generation import Generation, ReuqestText, generation_schema_example
 from app.utils import PreProcessor, parsing_generation_output
 
 router = APIRouter()
 generator = Generator()
 
+
 @router.post("/text", response_model=Generation)
-def generation_by_text(req: ReuqestText) -> Any:
+def generation_by_text(req: ReuqestText, live_mode: bool = True) -> Any:
     """
     Generate a response that includes a summary and questions from a text.\n
     A text (string) is required in the request body.
     """
+    # TODO: Remove code duplication using decorators, etc
+    if not live_mode:
+        return generation_schema_example
+
     result = generator.run(req.text)
     problems = parsing_generation_output(result["generation"])
 
     return {"summary": result["summary"], "problems": problems}
 
+
 @router.post("/pdf", response_model=Generation)
-def generation_by_pdf(upload_file: UploadFile) -> Any:
+def generation_by_pdf(upload_file: UploadFile, live_mode: bool = True) -> Any:
     """
     Generate a response that includes a summary and questions from a PDF file.\n
     A multipart PDF file is required in the request body.
     """
+    if not live_mode:
+        return generation_schema_example
+
     source = upload_file.file.read()
     pdf_preprocessor = PreProcessor(mode="pdf")
-    docs =  pdf_preprocessor.run(source)
+    docs = pdf_preprocessor.run(source)
     result = generator.run(docs)
     problems = parsing_generation_output(result["generation"])
 
@@ -36,15 +45,17 @@ def generation_by_pdf(upload_file: UploadFile) -> Any:
 
 
 @router.post("/video", response_model=Generation)
-async def generation_by_video(upload_file: UploadFile) -> Any:
+async def generation_by_video(upload_file: UploadFile, live_mode: bool = True) -> Any:
     """
     Generate a response that includes a summary and questions from a VIDEO file.\n
     A multipart VIDEO file is required in the request body.\n
     Currently, only MP4 video file format is supported.
     """
+    if not live_mode:
+        return generation_schema_example
     source = upload_file.file.read()
     video_preprocessor = PreProcessor(mode="video")
-    docs =  video_preprocessor.run(source)
+    docs = video_preprocessor.run(source)
     result = generator.run(docs)
     problems = parsing_generation_output(result["generation"])
 
--- app/schemas/generation.py
+++ app/schemas/generation.py
@@ -2,15 +2,49 @@ from typing import List
 
 from pydantic import BaseModel
 
+generation_schema_example = {
+    "summary": 'Amazon Elastic Compute Cloud (EC2) is a central part of Amazon Web Services, which is a cloud computing platform provided by Amazon.com. EC2 allows users to rent virtual computers and run their own computer applications on them. Users can boot the virtual machines, called instances, from Amazon Machine Images (AMI) and configure them with desired software. This web service encourages scalable application deployment by providing users with the ability to create, start, and terminate server instances as needed. Users are billed on an hourly basis for the running servers, hence the term "elastic." EC2 also offers control over the geographical location of instances, allowing for latency optimization and high-level redundancy. In November 2010, Amazon transitioned its own website to use EC2 and AWS.',
+    "problems": [
+        {
+            "problem_no": 1,
+            "question": "Amazon Elastic Compute Cloud (EC2)은 무엇인가요?",
+            "answer": "Amazon EC2는 사용자가 가상 컴퓨터를 임대하고 그 위에서 자신의 애플리케이션을 실행할 수 있는 클라우드 컴퓨팅 서비스입니다.",
+        },
+        {
+            "problem_no": 2,
+            "question": "EC2는 어떻게 확장 가능한 애플리케이션 배포를 가능하게 하나요?",
+            "answer": "EC2는 사용자가 필요에 따라 서버 인스턴스를 생성, 시작 및 종료할 수 있도록 해주어 애플리케이션 배포의 확장성을 제공합니다.",
+        },
+        {
+            "problem_no": 3,
+            "question": "Amazon Machine Images (AMI)은 무엇인가요?",
+            "answer": "AMI는 사용자가 EC2 인스턴스를 부팅하고 원하는 소프트웨어로 구성할 수 있는 가상 머신 템플릿입니다.",
+        },
+        {
+            "problem_no": 4,
+            "question": "EC2 사용에 대해 사용자는 어떻게 청구되나요?",
+            "answer": "사용자는 실행 중인 서버 인스턴스에 대해 시간당 요금을 지불합니다.",
+        },
+        {
+            "problem_no": 5,
+            "question": 'EC2에서 "elastic"이라는 용어는 무엇을 의미하나요?',
+            "answer": 'EC2에서 "elastic"이라는 용어는 수요에 따라 서버 인스턴스의 수를 쉽게 확장하거나 축소할 수 있는 능력을 의미합니다.',
+        },
+    ],
+}
+
 
 class ReuqestText(BaseModel):
     text: str
+
 
 class Problem(BaseModel):
     problem_no: int
     question: str
     answer: str
 
+
 class Generation(BaseModel):
     summary: str
     problems: List[Problem]
+    model_config = {"json_schema_extra": {"examples": [generation_schema_example]}}
